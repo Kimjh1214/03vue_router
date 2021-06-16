184e5e58cbd0334007737dc5384c97ef7e60bea1
Kimjh1214 <hzpotter1214@naver.com>
2021-06-16 14:01:18 +0900

    router

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <top-header></top-header>
+
+  
+  <bottom-footer></bottom-footer>
+  
+</template>
+
+<script>
+import header from './components/header.vue'
+import footer from './components/footer.vue'
+
+export default {
+  name:'app',
+  components:{
+    'top-header':header,
+    'bottom-footer':footer,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>LOGO</h1>
+      <ul class="gnb">
+          <li>home</li>
+          <li>company</li>
+          <li>product</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
